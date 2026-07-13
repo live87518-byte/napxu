f47f542750d0815df1111f485edb11e5d3ad57c9
live87518-byte <live87518@gmail.com>
2026-07-13 13:54:01 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    introTitle: 'NOTICE!',
-    introDesc: `Để Nhận Robux Miễn Phí<br>Bạn Cần Phải Làm Theo Các Bước Như Sau:<br> Bước 1 Nhập Tên Tài Khoản Robux Của Mình <br>Bước 2 Chọn Gói Nạp <br>Bước 3 Vượt Link Để Lấy Mã OTP<Br> Bước 4 Nhập Mã OTP vừa Vượt Link Và Nhấn Xác Nhận Và Đợi
-            <br>- Chúc Bạn Chơi Game vv - `,
-    btnIntro: 'Đóng',
-}
